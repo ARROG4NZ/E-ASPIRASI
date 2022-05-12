5c06731d716d6b115bf439c6b684427b9da4100a
habil <habiledgar111@gmail.com>
2022-05-12 14:01:54 +0700

    push

---
+++ app/Views/user/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+    body {
+        background: #951C21;
+        background: linear-gradient(45deg, rgba(149, 28, 33, 0.69), #D1484D);
+        min-height: 100%;
+    }
+
+    #warna {
+        background-color: white;
+    }
+    </style>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-fluid-12">
+            <P>row</P>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-fluid-12">
+            <p>row2</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-5" id="warna">
+            <p>coba</p>
+        </div>
+        <div class="col-lg-5" id="warna">
+            <p>coba2</p>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
