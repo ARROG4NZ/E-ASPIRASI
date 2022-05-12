04396df7915e008060681b8f3a55bec205103283
habil <habiledgar111@gmail.com>
2022-05-12 14:38:40 +0700

    user2

--- app/Views/user/home.php
+++ app/Views/user/home.php
@@ -12,32 +12,31 @@
     #warna {
         background-color: white;
     }
+
+    .my-auto {
+        margin-top: auto;
+        margin-bottom: auto;
+    }
     </style>
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-fluid-12">
-            <P>row</P>
+    <div class="jumbotron d-flex align-items-center min-vh-100">
+        <div class="container text-center">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-lg-5" id="warna">
+                    <p>coba</p>
+                </div>
+                <div class="col-lg-5" id="warna">
+                    <p>coba2</p>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-fluid-12">
-            <p>row2</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-5" id="warna">
-            <p>coba</p>
-        </div>
-        <div class="col-lg-5" id="warna">
-            <p>coba2</p>
-        </div>
-    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
