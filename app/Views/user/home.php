45e1a19586de761e8c57a2700bd8e9b84a4929d4
habil <habiledgar111@gmail.com>
2022-05-12 20:11:53 +0700

    push

--- app/Views/user/home.php
+++ app/Views/user/home.php
@@ -13,13 +13,64 @@
         background-color: white;
     }
 
+    #warna2 {
+        background-color: #F5CCCD;
+    }
+
+    #warna3 {
+        color: #D1484D;
+    }
+
+    #warna4 {
+        background-color: #D1484D;
+    }
+
+    .allborder-radius-custom {
+        border-radius: 30px;
+    }
+
+    .border-custom-left {
+        border-bottom-left-radius: 30px;
+        border-top-left-radius: 30px;
+        border-top-right-radius: 0px;
+        border-bottom-right-radius: 0px;
+    }
+
+    .border-custom-right {
+        border-top-right-radius: 30px;
+        border-top-left-radius: 0px;
+        border-bottom-left-radius: 0px;
+        border-bottom-right-radius: 30px;
+    }
+
+    .margin-left-panel {
+        margin: 40px 0px 80px 0px;
+    }
+
+    .container-custom {
+        text-align: center;
+    }
+
     .my-auto {
         margin-top: auto;
         margin-bottom: auto;
     }
+
+    h3 {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    h4 {
+        color: white;
+        font-family: 'Poppins', sans-serif;
+    }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- font poppins -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 
 </head>
 
@@ -27,11 +78,25 @@
     <div class="jumbotron d-flex align-items-center min-vh-100">
         <div class="container text-center">
             <div class="row justify-content-center align-items-center">
-                <div class="col-lg-5" id="warna">
-                    <p>coba</p>
+                <div class="col-5 border-custom-left" id="warna">
+                    <div class="card-body">
+                        <div class="container margin-left-panel">
+                            <h3 id="warna3">Selamat Datang</h3>
+                        </div>
+                        <div class="col-6 col-centered container margin-left-panel allborder-radius-custom" id="warna4">
+                            <h4>Membuat Aspirasi</h4>
+                        </div>
+                        <div class="col-6 col-centered container margin-left-panel allborder-radius-custom" id="warna4">
+                            <h4>Melihat Aspirasi</h4>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-lg-5" id="warna">
-                    <p>coba2</p>
+                <div class=" col-5 border-custom-right" id="warna2">
+                    <div class="card-body">
+                        <div class="container">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
