6dfc2a3fbbbc77d9b23394b793257fe41a58f88b
Muhammad Alfian <95150867+kamonegixx@users.noreply.github.com>
2022-05-16 17:31:25 +0700

    Add files via upload

---
+++ app/Controllers/Login.php
@@ -0,0 +1,37 @@
+<?php namespace App\Controllers;
+
+use App\Models\m_admin;
+
+class Login extends BaseController{
+    public function index(){
+     return view('login/index');
+    } 
+    
+    public function process()
+    {
+        $madmin = new m_admin();
+
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+        $cek = $madmin->get_data($username, $password);
+
+        if (($cek['username'] == $username) && ($cek['password'] == $password))
+        {
+            session()->set('username', $cek['username']);
+            session()->set('nama', $cek['nama']);
+            return redirect()->to(base_url('dataaspirasi'));
+        } 
+        else{
+            session()->setFlashdata('gagal', 'Username / Password salah');
+            return redirect()->back();
+        }
+    }
+
+    public function logout() 
+    {
+       session()->destroy();
+       return redirect()->to(base_url('login'));
+    }
+}
+?>
