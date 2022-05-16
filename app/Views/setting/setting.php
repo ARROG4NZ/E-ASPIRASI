271586ffd8283cb3fa771f94508f70117a77025a
Muhammad Alfian <95150867+kamonegixx@users.noreply.github.com>
2022-05-16 11:06:34 +0700

    Add files via upload

---
+++ app/Views/setting/setting.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+  <title>Setting</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+ <div class="container-fluid" style="padding: 0;">
+  <div class="row">
+    <!-- Sidear -->
+    <div class="col-md-2 d-none d-md-block sidebar" style="background-color: #D1484D;">
+      <div class="left-sidebar">
+        <div class="text-center" style="margin-top: 30px;">
+         <img src="public/profile.png" width="70px" height="70px" class="mr-2" style="border-radius: 50%;">
+         <span>Admin</span>
+       </div>
+       <hr>
+       <ul class="nav flex-column sidebar-nav" style="margin-top: 60px;">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">
+              <img src="public/data_aspirasi.jpg" width="20px" class="mr-2">
+              Data Aspirasi
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="setting.html">
+              <img src="setting.jpg" width="20px" class="mr-2">
+              Setting
+            </a>
+          </li>
+        </ul>
+    </div>
+  </div>
+</div>
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="margin-top: -25px;">
+  <div class="header">
+
+    <div class="title">
+      <h3 class="font-weight-bold">Setting</h3>
+    </div>
+    
+  </div>
+  <!-- <hr> -->
+  <div class="setting text-center">
+    <img src="profile.png" width="140px">
+
+    <form style="margin-top: 15px;">
+      <div class="form-group">
+        <input type="text" name="username" placeholder="Admin">
+      </div>
+      <div class="form-group">
+        <input type="text" name="password" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <input type="text" name="password_baru" placeholder="Password Baru">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-simpan mt-2">Simpan</button>
+      </div>
+    </form>
+  </div>
+</main>
+
+<footer>
+
+</footer>
+
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
