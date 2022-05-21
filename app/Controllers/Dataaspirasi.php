fa652d6b9ceef6aa24292a716a432598b161c593
habil <habiledgar111@gmail.com>
2022-05-21 13:09:46 +0700

    mengedit controler

---
+++ app/Controllers/Dataaspirasi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\m_aspirasi;
+
+class dataaspirasi extends BaseController
+{
+   public function index()
+   {
+      if(session()->get('username')==''){
+         session()->setFlashdata('gagal','Anda belum login');
+         return redirect()->to(base_url('login'));
+      }
+      $dataaspirasi = new m_aspirasi();
+      $data = $dataaspirasi->getallaspriasi();
+      var_dump('data');
+   }
+
+}
