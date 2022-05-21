5384df811e00b6bab97dacbea5f8487430bb358b
Muhammad Alfian <95150867+kamonegixx@users.noreply.github.com>
2022-05-21 12:29:22 +0700

    tes controller halaman utama

---
+++ app/Controllers/Dataaspirasi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class dataaspirasi extends BaseController
+{
+   public function index()
+   {
+      if(session()->get('username')==''){
+         session()->setFlashdata('gagal','Anda belum login');
+         return redirect()->to(base_url('login'));
+      }
+      return view('coba');
+   }
+
+}
