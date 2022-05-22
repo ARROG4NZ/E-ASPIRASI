90c03ee11f2719ba6ae33f1ae54cfbab920fd5c1
Muhammad Alfian <95150867+kamonegixx@users.noreply.github.com>
2022-05-22 10:01:19 +0700

    Update setting.php

--- app/Controllers/setting.php
+++ app/Controllers/setting.php
@@ -8,4 +8,17 @@ class setting extends BaseController
     {
         return view('setting/setting');
     }
+    
+    public function changepass(){
+        $m_setting = new m_setting();
+        $change = $m_setting->getuser($this->session->userdata('username'));
+        
+        if($this->input->post('password_lama') == $change->password){
+            if($this->$m_setting->changepass()){
+                $this->session->set_flashdata('msg', 'password berhasil diganti');
+            }
+            else{$this->session->set_flashdata('msg', 'password gagal diganti');}
+        }
+        else{$this->session->set_flashdata('msg', 'password lama salah');}
+    }
 }
